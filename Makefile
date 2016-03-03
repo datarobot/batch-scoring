1d4d91a54755813509afdbb7360d10324305ab65
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-03-03 15:27:35 +0200

    Add travis-ci support
    * cleanup repo
    * add --yes/--no cmdline options
    * fix fake server response
    
    Drop datarobot sdk dependency
    
    Install pymongo: need for bson objects
    
    Use 'always no' mode in tests
    
    Disable a test
    
    Install docutils (required for setup.py check -rms)
    
    Debug
    
    More verbose
    
    More debug
    
    User proper samples count
    
    Try another way to have proper batch size
    
    Remove redundant prints

--- Makefile
+++ Makefile
@@ -3,6 +3,18 @@
 flake8:
 	flake8 datarobot_batch_scoring
 
-test:
-	pip install -r requirements.txt
-	py.test -v --junit-xml=testResults.xml tests/
+.install-deps: requirements.txt
+	pip install -U -r requirements.txt
+	touch .install-deps
+
+.install: .install-deps $(shell find datarobot_batch_scoring -type f)
+	pip install -e .
+	touch .install
+
+test: .install
+	py.test -v tests/
+
+cov cover coverage: .install
+	py.test -v --cov=datarobot_batch_scoring \
+            --cov-report=term --cov-report=html tests/
+	@echo "open file://`pwd`/htmlcov/index.html"
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,4 +1,6 @@
+from __future__ import print_function
 import subprocess
+import sys
 import requests
 from bson import ObjectId
 
@@ -12,13 +14,15 @@ def test_args_from_subprocess(live_server):
                  ' {model_id}'
                  ' tests/fixtures/temperatura_predict.csv'
                  ' --n_samples 10'
-                 ' --n_concurrent 1').format(webhost=live_server.url(),
-                                             username='username',
-                                             password='passowrd',
-                                             project_id=ObjectId(),
-                                             model_id=ObjectId())
+                 ' --n_concurrent 1'
+                 ' --no').format(webhost=live_server.url(),
+                                 username='username',
+                                 password='passowrd',
+                                 project_id=ObjectId(),
+                                 model_id=ObjectId())
 
-    assert 0 == subprocess.call(arguments.split(' '))
+    assert 0 == subprocess.call(arguments.split(' '), stdout=sys.stdout,
+                                stderr=subprocess.STDOUT)
 
 
 def test_live_server(live_server):
