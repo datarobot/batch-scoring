30a1b71f5e00acb0362a22599a58f90cfe08963a
mckaymatt <mckaymatt@gmail.com>
2017-02-22 11:48:42 -0500

    send OSX  builds to s3 on PR and tag

--- offline_install_scripts/travis_pyinstaller_osx.sh
+++ offline_install_scripts/travis_pyinstaller_osx.sh
@@ -2,7 +2,14 @@
 set -e
 set -x
 # bundles up the OSX PyInstaller build
-TRAVIS_TAG=$1
+TRAVIS_COMMIT=$1
+TRAVIS_TAG=$2
+if [[ $TRAVIS_TAG ]]
+then 
+    VERS=$TRAVIS_TAG
+else
+    VERS=$TRAVIS_COMMIT
+fi
 REPO_BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 cd $REPO_BASE
 mkdir -p dist/datarobot_batch_scoring_executables
@@ -10,4 +17,7 @@ mkdir -p dist/datarobot_batch_scoring_executables
 cp BATCH_SCORING_EXECUTABLE_README.txt dist/datarobot_batch_scoring_executables/
 cd dist
 cp batch_scoring batch_scoring_sse datarobot_batch_scoring_executables/
-tar -cf datarobot_batch_scoring_"${TRAVIS_TAG}"_executables.OSX.x86_64.tar datarobot_batch_scoring_executables
+tar -cf datarobot_batch_scoring_"${VERS}"_executables.OSX.x86_64.tar datarobot_batch_scoring_executables
+mkdir OSX
+mv datarobot_batch_scoring_"${VERS}"_executables.OSX.x86_64.tar OSX/
+
