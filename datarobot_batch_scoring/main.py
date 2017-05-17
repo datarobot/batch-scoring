0501bb7e9e62c3db2b8145505b9a06c62cd908df
Dallin Akagi <dsakagi@gmail.com>
2017-05-17 16:38:44 -0500

    Fix lint

--- datarobot_batch_scoring/main.py
+++ datarobot_batch_scoring/main.py
@@ -189,8 +189,8 @@ def parse_args(argv, standalone=False):
                         help='Override the maximum field size. May be '
                              'necessary for datasets with very wide text '
                              'fields, but can lead to memory issues. Use '
-                             '`max` for the system maximum value, otherwise specify '
-                             'an integer for the number of bytes.')
+                             '`max` for the system maximum value, otherwise '
+                             'specify an integer for the number of bytes.')
     misc_gr = parser.add_argument_group('Miscellaneous')
     misc_gr.add_argument('-y', '--yes', dest='prompt', action='store_true',
                          help="Always answer 'yes' for user prompts")
@@ -233,9 +233,9 @@ def parse_args(argv, standalone=False):
 
 
 def _parse_field_size_limit(fsl):
-    """Users can either (1) not specify (i.e. will be `None`) in which case 
+    """Users can either (1) not specify (i.e. will be `None`) in which case
     we will use the system default, (2) use the string `max` in which case we
-    will use sys.maxsize, or (3) specify an integer for how many bytes a 
+    will use sys.maxsize, or (3) specify an integer for how many bytes a
     field can take up
     """
     if fsl is None:
